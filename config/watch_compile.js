8ec614ccb69eb8a024ea01dcae9d737f474c148c
Dernbu <63487502+Dernbu@users.noreply.github.com>
2022-12-01 01:35:23 +0800

    Enable build on watch

--- config/watch_compile.js
+++ config/watch_compile.js
@@ -1,7 +1,10 @@
 const watch = require('node-watch');
 const fs = require('fs');
-const { exec } = require("child_process");
 const toml = require('toml');
+const http = require('http');
+const { exec, execSync } = require("child_process");
+
+let webserver_served = false;
 
 const configData = toml.parse(fs.readFileSync("./config/config.toml"));
 
@@ -9,22 +12,75 @@ const configData = toml.parse(fs.readFileSync("./config/config.toml"));
 // If entryPath = configData.entry = ".src/main.cpp",
 // If entryFile = "main.cpp",
 // If entryFileName = "main"
-const entryPath = configData.entry.dir + "/" + configData.entry.file;
-const entryFile = configData.entry.file;
+const entryDir = configData.entry.dir;
+const entryPath = configData.entry.dir + "/" + configData.entry.infile;
+const entryFile = configData.entry.infile;
 const entryFileName = entryFile.substring(0, entryFile.lastIndexOf("."));
 
-// Run Command
-const params = configData.command.params.join(" ").replaceAll("[name]", entryFileName);
-const command = `emcc ${entryPath} ${params}`;
+const outputDir = replacePlaceholders(configData.output.dir);
+const outputPath = replacePlaceholders(configData.output.dir + "/" + configData.output.outfile);
 
-exec(command, (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`Compiled successfully! ${stdout}`);
+console.clear();
+
+build();
+
+// Run Command on file change
+watch(entryDir, (evt, name) => {
+
+    console.clear();
+    console.log(`${name} changed (${evt}), compiling...\n`);
+
+    build();
+
 });
+
+/**
+ * Build emscripten file
+ */
+function build() {
+    const params = replacePlaceholders(configData.command.params.join(" "));
+    const command = `emcc ${entryPath} ${params}`;
+
+
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`Compiled ${entryPath} successfully! ${stdout}`);
+        serve();
+        console.log(`Watching ${entryDir} for changes...`);
+    });
+}
+
+/**
+ * Serve html file in a webserver
+ */
+function serve() {
+    const PORT = 8080;
+    if (webserver_served) {
+        console.log(`Server available on: http://localhost:${PORT}`);
+        return;
+    }
+
+    fs.readFile(outputPath, (err, html) => {
+        if (err) throw err;
+
+        const server = http.createServer((request, response) => {
+            response.writeHeader(200, { "Content-Type": "text/html" });
+            response.write(html);
+            response.end();
+        }).listen(PORT);
+        
+        console.log(`Server available on: http://localhost:${PORT}`);
+    })
+    webserver_served = true;
+}
+
+function replacePlaceholders(str) {
+    return str.replaceAll("[name]", entryFileName);
+}
